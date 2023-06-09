f6ce438aafc35acfc748a59bd3881a8dec238b06
wasabi315 <wasabi.nvim.st@gmail.com>
2023-06-09 16:50:14 +0200

    CPS'ed ObjectF

--- haskell/shallow-handlers/app/Main.hs
+++ haskell/shallow-handlers/app/Main.hs
@@ -38,16 +38,17 @@ send = liftF . liftCoyoneda
 
 -- The base functor of Object
 
-newtype ObjectF f g a = ObjectF (forall x. f x -> g (x, a))
+-- newtype ObjectF f g a = ObjectF (forall x. f x -> g (x, a))
+newtype ObjectF f g a = ObjectF (forall x r. f x -> (x -> a -> g r) -> g r)
   deriving stock (Functor)
 
 type instance Base (Object f g) = ObjectF f g
 
-instance Functor g => Recursive (Object f g) where
-  project (Object f) = ObjectF f
+instance Monad g => Recursive (Object f g) where
+  project (Object f) = ObjectF \m k -> f m >>= uncurry k
 
-instance Functor g => Corecursive (Object f g) where
-  embed (ObjectF f) = Object f
+instance Monad g => Corecursive (Object f g) where
+  embed (ObjectF f) = Object \m -> f m \x obj -> pure (x, obj)
 
 -- Shallow handler defined with Cofree
 
@@ -55,31 +56,28 @@ type SH f m a b = Cofree (ObjectF f m) (a -> m b)
 
 pattern SH ::
   (a -> m b) ->
-  (forall x. f x -> m (x, SH f m a b)) ->
+  (forall x r. f x -> (x -> SH f m a b -> m r) -> m r) ->
   SH f m a b
 pattern SH {retc, effc} = retc :< ObjectF effc
 
 {-# COMPLETE SH #-}
 
 unfoldSH ::
-  Monad m =>
   (s -> a -> m b) -> -- state -> value handler
-  (forall x. s -> f x -> m (x, s)) -> -- state -> (effect handler x state)
+  (forall x r. s -> f x -> (x -> s -> m r) -> m r) -> -- state -> (effect handler x state)
   s ->
   SH f m a b
 unfoldSH f g = unfold \s -> (f s, ObjectF (g s))
 
 deep :: Monad m => (a -> m b) -> (forall x. f x -> m x) -> SH f m a b
-deep retc effc = sh where sh = SH {retc, effc = fmap (,sh) . effc}
+deep retc effc = sh where sh = SH {retc, effc = \m k -> effc m >>= flip k sh}
 
 fromNat :: Monad m => (forall x. f x -> m x) -> SH f m a a
 fromNat = deep pure
 
 runFreer :: Monad m => SH f m a b -> Freer f a -> m b
 runFreer SH {retc} (Pure a) = retc a
-runFreer SH {effc} (Bind m k) = do
-  (b, sh) <- effc m
-  runFreer sh (k b)
+runFreer SH {effc} (Bind m k) = effc m \x sh -> runFreer sh (k x)
 
 -- State
 
@@ -96,9 +94,9 @@ put = send . Put
 evalState1 :: Freer (State s) a -> s -> a
 evalState1 = \m s -> runIdentity $ runFreer (unfoldSH (const pure) h s) m
   where
-    h :: s -> State s a -> Identity (a, s)
-    h s Get = pure (s, s)
-    h _ (Put s) = pure ((), s)
+    h :: s -> State s a -> (a -> s -> Identity r) -> Identity r
+    h s Get k = k s s
+    h _ (Put s) k = k () s
 
 evalState2 :: Freer (State s) a -> s -> a
 evalState2 = Mtl.evalState . runFreer (fromNat h)
@@ -134,8 +132,8 @@ alternating :: Freer CoinFlip a -> a
 alternating = runIdentity . runFreer t
   where
     t, f :: SH CoinFlip Identity a a
-    t = SH {retc = pure, effc = \CoinFlip -> pure (True, f)}
-    f = SH {retc = pure, effc = \CoinFlip -> pure (False, t)}
+    t = SH {retc = pure, effc = \CoinFlip k -> k True f}
+    f = SH {retc = pure, effc = \CoinFlip k -> k False t}
 
 testCoinFlip :: IO ()
 testCoinFlip = do
