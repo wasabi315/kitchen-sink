40eb8eede1661dcbb7e31af49365468069ffbfa3
wasabi315 <wasabi.nvim.st@gmail.com>
2023-07-16 12:49:57 +0900

    Update c_printer

--- ocaml/c_printer/main.ml
+++ ocaml/c_printer/main.ml
@@ -2,9 +2,9 @@ open Format
 
 module C_printer : sig
   val output : formatter -> (unit -> unit) -> unit
-  val func : ('a, formatter, unit, (unit -> unit) -> unit) format4 -> 'a
-  val __ : ('a, formatter, unit) format -> 'a
-  val if' : ('a, formatter, unit, (unit -> unit) -> unit) format4 -> 'a
+  val fn : ('a, formatter, unit, (unit -> unit) -> unit) format4 -> 'a
+  val ( ! ) : ('a, formatter, unit) format -> 'a
+  val ifel : ('a, formatter, unit, (unit -> unit) -> (unit -> unit) -> unit) format4 -> 'a
 end = struct
   let proto_ctx, func_ctx = 0, 1
   let q = Array.init 2 (fun _ -> Queue.create ())
@@ -12,47 +12,106 @@ end = struct
   let output ppf f =
     Array.iter Queue.clear q;
     f ();
-    fprintf ppf "@[<v>";
+    fprintf ppf "@[<v>#include <stdio.h>";
     Array.iter (Queue.iter (( |> ) ppf)) q;
     fprintf ppf "@]@."
   ;;
 
-  let add_to ctx fmt = kdprintf (fun p -> Queue.add p q.(ctx)) fmt
+  let print_to ctx = kdprintf (fun p -> Queue.add p q.(ctx))
 
-  let func fmt =
+  let fn fmt =
     kdprintf
       (fun p f ->
-        add_to proto_ctx "@,%t;" p;
-        add_to func_ctx "@,@[<v 4>%t {" p;
+        print_to proto_ctx "@,%t;" p;
+        print_to func_ctx "@,@[<v 4>%t {" p;
         f ();
-        add_to func_ctx "@]@,}")
+        print_to func_ctx "@]@,}")
       fmt
   ;;
 
-  let __ fmt = kdprintf (add_to func_ctx "@,%t;") fmt
+  let ( ! ) fmt = kdprintf (print_to func_ctx "@,%t;") fmt
 
-  let if' fmt =
+  let ifel fmt =
     kdprintf
-      (fun p f ->
-        add_to func_ctx "@,@[<v 4>if (%t)" p;
-        f ();
-        add_to func_ctx "@]@,}")
+      (fun p t e ->
+        print_to func_ctx "@,@[<v 4>if (%t) {" p;
+        t ();
+        print_to func_ctx "@]@,@[<v 4>} else {";
+        e ();
+        print_to func_ctx "@]@,}")
       fmt
   ;;
 end
 
+module Toy_lang = struct
+  type program =
+    { main : string
+    ; main_arg : int
+    ; fns : (string * string * expr) list
+    }
+
+  and expr =
+    | Int of int
+    | Var of string
+    | Add of expr * expr
+    | Sub of expr * expr
+    | IsZero of expr
+    | Call of string * expr
+    | If of expr * expr * expr
+end
+
+module Codegen = struct
+  let gen_var =
+    let count = ref 0 in
+    fun () ->
+      count := !count + 1;
+      sprintf "v%d" !count
+  ;;
+
+  open C_printer
+
+  let gen_body =
+    let rec gen_expr = function
+      | Toy_lang.Int n -> dprintf "%d" n
+      | Var v -> dprintf "%s" v
+      | Add (l, r) -> dprintf "(%t + %t)" (gen_expr l) (gen_expr r)
+      | Sub (l, r) -> dprintf "(%t - %t)" (gen_expr l) (gen_expr r)
+      | IsZero e -> dprintf "(%t == 0)" (gen_expr e)
+      | Call (f, e) -> dprintf "%s(%t)" f (gen_expr e)
+      | If (c, t, e) ->
+        let v = gen_var () in
+        !"int %s" v;
+        (ifel "%t" (gen_expr c))
+          (fun () -> !"%s = %t" v (gen_expr t))
+          (fun () -> !"%s = %t" v (gen_expr e));
+        dprintf "%s" v
+    in
+    fun expr -> !"return %t" (gen_expr expr)
+  ;;
+
+  let gen_fn (fn_name, param_name, body) =
+    fn "int %s(int %s)" fn_name param_name (fun () -> gen_body body)
+  ;;
+
+  let gen_program prog =
+    fn "int main(void)" (fun () ->
+      !"printf(\"%%d\\n\", %s(%d))" prog.Toy_lang.main prog.main_arg;
+      !"return 0");
+    List.iter gen_fn prog.fns
+  ;;
+
+  let f ppf prog = output ppf (fun () -> gen_program prog)
+end
+
 let () =
-  let open C_printer in
-  let program n =
-    func "int main(void)" (fun () ->
-      __ "printf(\"%%d\\n\", even(%d))" n;
-      __ "return 0");
-    func "int even(int n)" (fun () ->
-      if' "n == 0" (fun () -> __ "return 1");
-      __ "return odd(n - 1)");
-    func "int odd(int n)" (fun () ->
-      if' "n == 0" (fun () -> __ "return 0");
-      __ "return even(n - 1)")
+  let program =
+    { Toy_lang.main = "even"
+    ; main_arg = 100
+    ; fns =
+        [ "even", "n", If (IsZero (Var "n"), Int 1, Call ("odd", Sub (Var "n", Int 1)))
+        ; "odd", "n", If (IsZero (Var "n"), Int 0, Call ("even", Sub (Var "n", Int 1)))
+        ]
+    }
   in
-  output std_formatter (fun () -> program 100)
+  Codegen.f std_formatter program
 ;;
