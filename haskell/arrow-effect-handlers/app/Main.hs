e9e570f5a783e4964972a9b9f7069adc79a69422
wasabi315 <wasabi.nvim.st@gmail.com>
2023-11-25 15:20:25 +0900

    Update arrow-effect-handlers

--- haskell/arrow-effect-handlers/app/Main.hs
+++ haskell/arrow-effect-handlers/app/Main.hs
@@ -1,111 +1,83 @@
 {-# LANGUAGE Arrows #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DisambiguateRecordFields #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
--- Effect handlers for arrows
--- Ref: https://www.kurims.kyoto-u.ac.jp/~tsanada/papers/jssst2023-arrow-handler.pdf
+module Main (module Main) where
 
 import Control.Arrow
-import Control.Category
-import Control.Exception (assert)
+import Control.Arrow.Effect
+import Control.Arrow.Effect.ArrowApply qualified as ArrowApply
+import Control.Arrow.Effect.Church qualified as Church
+import Control.Arrow.Effect.MoreInputs qualified as MoreInputs
 import Prelude hiding (id, (.))
 
-{-
+data SuccRes i o where
+  SuccRes :: SuccRes x x
 
-  λᵒ x. Q         | proc x -> Q
-  L ● M           | L -< M
-  [M]             | returnA -< M
-  let x <= P in Q | do { x <- P; Q }
-  op(M)           | Eff op -< M
-  handle R with H | (| (handleWith H) R |) <additional input>
+runSuccRes :: (Arrow a) => Eff SuccRes b Int -> a b Int
+runSuccRes =
+  handleWith
+    Handler
+      { valh = returnA,
+        effh = \SuccRes k -> k >>> arr succ
+      }
 
--}
+runSuccResC :: (Arrow a) => Church.Eff SuccRes b Int -> a b Int
+runSuccResC =
+  Church.handleWith
+    Handler
+      { valh = returnA,
+        effh = \SuccRes k -> k >>> arr succ
+      }
 
-data Eff op b c where
-  Arr :: (b -> c) -> Eff op b c
-  Op :: op b c -> Eff op b c
-  Comp :: Eff op b c -> Eff op c d -> Eff op b d
-  First :: Eff op b c -> Eff op (b, d) (c, d)
+runSuccResAA :: (ArrowApply a) => ArrowApply.Eff SuccRes b Int -> a b Int
+runSuccResAA =
+  ArrowApply.handleWith
+    ArrowApply.Handler
+      { valh = returnA,
+        effh = \SuccRes k -> k >>> arr succ
+      }
 
-instance Category (Eff op) where
-  id = Arr id
-  (.) = flip Comp
-
-instance Arrow (Eff op) where
-  arr = Arr
-  first = First
-
-data Handler op a e' r r' = Handler
-  { valh :: a (e', r) r',
-    effh :: forall i o x. op i o -> a (o, e', x) r' -> a (i, e', x) r'
-  }
-
-interpret :: (Arrow a) => (forall i o. op i o -> a i o) -> Handler op a () b b
-interpret f =
-  Handler
-    { valh = arr snd,
-      effh = \op k -> proc (x, b', z) -> do
-        y <- f op -< x
-        k -< (y, b', z)
-    }
-
-handleWith ::
-  forall op a e s e' r r'.
-  (Arrow a) =>
-  Handler op a e' r r' ->
-  Eff op (e, s) r ->
-  a (e, (e', s)) r'
-handleWith Handler {..} m = proc (e, (e', s)) -> do
-  go m (proc (r, e', ()) -> valh -< (e', r)) -< ((e, s), e', ())
-  where
-    go :: forall i o x. Eff op i o -> a (o, e', x) r' -> a (i, e', x) r'
-    go (Arr f) k = proc (i, e', x) -> k -< (f i, e', x)
-    go (Op op) k = effh op k
-    go (Comp f g) k = go f $ go g k
-    go (First f) k = proc ((i, x'), e', x) ->
-      go f (proc (o, e', (x', x)) -> k -< ((o, x'), e', x)) -< (i, e', (x', x))
-
--- Example
-
-data SuccPred b c where
-  Succ :: SuccPred Int Int
-  Pred :: SuccPred Int Int
-
-runSucc :: (Arrow a) => Eff SuccPred b c -> a b c
-runSucc m = proc b -> (| (handleWith h) (m -< b) |) ()
-  where
-    h = interpret \case
-      Succ -> arr succ
-      Pred -> arr pred
-
-testSucc :: (Arrow a) => a Int Int
-testSucc = runSucc proc n -> do
-  m <- Op Succ -< n
-  o <- Op Pred -< n
+testSuccRes :: Int -> Int
+testSuccRes = runSuccRes proc n -> do
+  m <- Op SuccRes -< 2 * n
+  o <- Op SuccRes -< m
   returnA -< m + o
 
-data State s b c where
-  Get :: State s b s
+testSuccResC :: Int -> Int
+testSuccResC = runSuccResC proc n -> do
+  m <- Church.op SuccRes -< 2 * n
+  o <- Church.op SuccRes -< m
+  returnA -< m + o
+
+testSuccResAA :: Int -> Int
+testSuccResAA = runSuccResAA proc n -> do
+  m <- Op SuccRes -< 2 * n
+  o <- Op SuccRes -< m
+  returnA -< m + o
+
+data State s i o where
+  Get :: State s () s
   Set :: State s s ()
 
-runState :: forall a s b c. (Arrow a) => Eff (State s) b c -> a (s, b) (s, c)
-runState m = proc (s, a) -> (| (handleWith h) (m -< a) |) s
+runState :: forall a s b c. (Arrow a) => Eff (State s) b c -> a (b, s) (c, s)
+runState m = proc (b, s) -> (| (MoreInputs.handleWith h) (m -< b) |) s
   where
-    h :: Handler (State s) a s r (s, r)
+    h :: MoreInputs.Handler (State s) a s c (c, s)
     h =
-      Handler
+      MoreInputs.Handler
         { valh = returnA,
           effh = \cases
-            Get k -> proc (_, s, x) -> k -< (s, s, x)
-            Set k -> proc (s', _, x) -> k -< ((), s', x)
+            Get k -> proc ((), s, x) -> k -< (s, s, x)
+            Set k -> proc (s, _, x) -> k -< ((), s, x)
         }
 
-testState :: (Arrow a) => a (Int, Int) (Int, Int)
+testState :: (Int, Int) -> (Int, Int)
 testState = runState proc n -> do
   s <- Op Get -< ()
   Op Set -< n + s
@@ -113,9 +85,7 @@ testState = runState proc n -> do
 
 main :: IO ()
 main = do
-  assertIO $ testSucc 10 == 20
-  assertIO $ testState (10, 20) == (30, 10)
-  putStrLn "OK"
-
-assertIO :: Bool -> IO ()
-assertIO = flip assert (pure ())
+  print $ testSuccRes 10
+  print $ testSuccResC 10
+  print $ testSuccResAA 10
+  print $ testState (2, 40)
---
+++ haskell/arrow-effect-handlers/src/Control/Arrow/Effect.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TupleSections #-}
+
+module Control.Arrow.Effect
+  ( Eff (..),
+    Handler (..),
+    handleWith,
+    interpret,
+  )
+where
+
+import Control.Arrow
+import Control.Category
+import Text.Show.Functions ()
+import Prelude hiding (id, (.))
+
+--------------------------------------------------------------------------------
+-- Misc
+
+unit :: (Arrow a) => a b (b, ())
+unit = arr (,())
+
+ununit :: (Arrow a) => a (b, ()) b
+ununit = arr fst
+
+assoc :: (Arrow a) => a ((b, c), d) (b, (c, d))
+assoc = arr \((b, c), d) -> (b, (c, d))
+
+unassoc :: (Arrow a) => a (b, (c, d)) ((b, c), d)
+unassoc = arr \(b, (c, d)) -> ((b, c), d)
+
+--------------------------------------------------------------------------------
+-- Effectful Arrows and Handlers
+
+data Eff op b c where
+  Arr :: (b -> c) -> Eff op b c
+  Op :: op b c -> Eff op b c
+  Comp :: Eff op c d -> Eff op b c -> Eff op b d
+  First :: Eff op b c -> Eff op (b, d) (c, d)
+
+-- for debugging
+deriving instance (forall i o. Show (op i o)) => Show (Eff op b c)
+
+instance Category (Eff op) where
+  id = Arr id
+  Comp f g . h = f . (g . h) -- for associativity
+  f . g = Comp f g
+
+instance Arrow (Eff op) where
+  arr = Arr
+  first = First
+
+data Handler op a c d = Handler
+  { valh :: a c d,
+    effh :: forall i o x. op i o -> a (o, x) d -> a (i, x) d
+  }
+
+handleWith ::
+  forall op a b c d.
+  (Arrow a) =>
+  Handler op a c d ->
+  Eff op b c ->
+  a b d
+handleWith Handler {..} m = unit >>> run m (ununit >>> valh)
+  where
+    run :: forall i o x. Eff op i o -> a (o, x) d -> a (i, x) d
+    {-
+       i  .+++++++. o  .------.        i  .-------. o  .------.
+      --->| Arr f |--->|      |       --->| arr f |--->|      |
+          '+++++++'    | cont |  ==>      '-------'    | cont |
+      ---------------->|      |       ---------------->|      |
+       x            x  '------'        x            x  '------'
+    -}
+    run (Arr f) cont = arr (first f) >>> cont
+    {-
+                                          .-----------------.
+       i  .+++++++. o  .------.        i  |        .------. |
+      --->| Eff e |--->|      |       --->|        |      | |
+          '+++++++'    | cont |  ==>      | effh e | cont | |
+      ---------------->|      |       --->|        |      | |
+       x            x  '------'        x  |        '------' |
+                                          '-----------------'
+    -}
+    run (Op e) cont = effh e cont
+    {-
+                                                     .-------------------.
+       i  .++++++++++. o  .------.        i  .+++. j |  .+++. o .------. |
+      --->| Comp f g |--->|      |       --->| g |----->| f |-->|      | |
+          '++++++++++'    | cont |  ==>      '+++'   |  '+++'   | cont | |
+      ------------------->|      |       ---------------------->|      | |
+       x               x  '------'        x        x |       x  '------' |
+                                                     '-------------------'
+    -}
+    run (Comp f g) cont = run g (run f cont)
+    {-
+                                                                .------------------.
+      (i,y) .+++++++++. (o,y) .------.       (i,y)   i .+++. o  |   (o,y) .------. |
+      =====>| First f |======>|      |       ====+---->| f |--------+====>|      | |
+            '+++++++++'       | cont |  ==>       \    '+++'    |  /      | cont | |
+      ----------------------->|      |       ------+==============+------>|      | |
+       x                   x  '------'        x        (y,x)    |      x  '------' |
+                                                                '------------------'
+    -}
+    run (First f) cont = assoc >>> run f (unassoc >>> cont)
+
+interpret :: (Arrow a) => (forall i o. op i o -> a i o) -> Eff op b c -> a b c
+interpret f =
+  handleWith Handler {valh = returnA, effh = \e k -> first (f e) >>> k}
