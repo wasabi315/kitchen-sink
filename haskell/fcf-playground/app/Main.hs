91b2d4fef0b7caac6361adc625a69a268b23ef64
wasabi315 <wasabi.nvim.st@gmail.com>
2023-12-06 11:39:07 +0900

    Update fcf-playground

--- haskell/fcf-playground/app/Main.hs
+++ haskell/fcf-playground/app/Main.hs
@@ -12,35 +12,45 @@ import GHC.TypeNats qualified as TN
 --------------------------------------------------------------------------------
 -- Type-level Infinite Streams
 
-data Stream a = a ::: Exp (Stream a)
+-- Exp can be seen as a thunk that can be forced by Eval.
+data Stream a = a :> Exp (Stream a)
 
 -- Elimination
 
 data SHead :: Stream a -> Exp a
 
-type instance Eval (SHead (x '::: _)) = x
+type instance Eval (SHead (x ':> _)) = x
 
 data STail :: Stream a -> Exp (Stream a)
 
-type instance Eval (STail (_ '::: xs)) = Eval xs
+type instance Eval (STail (_ ':> xs)) = Eval xs
 
 data Nth :: Nat -> Stream a -> Exp a
 
 type instance Eval (Nth n xs) = NthImpl n xs
 
 type family NthImpl (i :: Nat) (xs :: Stream a) :: a where
-  NthImpl 0 (x '::: _) = x
-  NthImpl i (_ '::: xs) = NthImpl (i TN.- 1) (Eval xs)
+  NthImpl 0 (x ':> _) = x
+  NthImpl i (_ ':> xs) = NthImpl (i TN.- 1) (Eval xs)
+
+data Take :: Nat -> Stream a -> Exp [a]
+
+type instance Eval (Take n xs) = TakeImpl n xs
+
+type family TakeImpl (n :: Nat) (xs :: Stream a) :: [a] where
+  TakeImpl 0 _ = '[]
+  TakeImpl n (x ':> xs) = x ': TakeImpl (n TN.- 1) (Eval xs)
 
 -- Construction
 
 data Repeat :: a -> Exp (Stream a)
 
-type instance Eval (Repeat x) = x '::: Repeat x
+type instance Eval (Repeat x) = x ':> Repeat x
 
-data Iterate :: (a -> Exp a) -> a -> Exp (Stream a)
+data Iterate' :: (a -> Exp a) -> a -> Exp (Stream a)
 
-type instance Eval (Iterate f x) = x '::: Iterate f (f @@ x)
+-- (f x) is forced thus the name
+type instance Eval (Iterate' f x) = x ':> Iterate' f (f @@ x)
 
 data Unfold :: (b -> Exp (a, b)) -> b -> Exp (Stream a)
 
@@ -48,19 +58,19 @@ data UnfoldCase :: (b -> Exp (a, b)) -> (a, b) -> Exp (Stream a)
 
 type instance Eval (Unfold f b) = Eval (UnfoldCase f (f @@ b))
 
-type instance Eval (UnfoldCase f '(a, b)) = a '::: Unfold f b
+type instance Eval (UnfoldCase f '(a, b)) = a ':> Unfold f b
 
 -- Functor
 
-type instance Eval (Map f (x '::: xs)) = (f @@ x) '::: Map f (Eval xs)
+type instance Eval (Map f (x ':> xs)) = (f @@ x) ':> (Map f =<< xs)
 
 -- Examples
 
 data Nats :: Exp (Stream Nat)
 
-type instance Eval Nats = Eval (Iterate ((+) 1) 0)
+type instance Eval Nats = Eval (Iterate' ((+) 1) 0)
 
-testNats :: Eval (Nth 100 =<< Nats) :~: 100
+testNats :: Eval (Take 10 =<< Nats) :~: '[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 testNats = Refl
 
 data Fibs :: Exp (Stream Nat)
