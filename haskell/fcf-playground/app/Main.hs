c2aa6e7bb55e23aac69bf489e5dd46b24ec4e5ab
wasabi315 <wasabi.nvim.st@gmail.com>
2023-12-06 22:30:55 +0900

    Update fcf-playground

--- haskell/fcf-playground/app/Main.hs
+++ haskell/fcf-playground/app/Main.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 
+import Data.Type.Bool qualified as TB
 import Data.Type.Equality
 import Fcf
 import Fcf.Data.Nat
@@ -70,8 +71,8 @@ data Nats :: Exp (Stream Nat)
 
 type instance Eval Nats = Eval (Iterate' ((+) 1) 0)
 
-testNats :: Eval (Take 10 =<< Nats) :~: '[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-testNats = Refl
+_testNats :: Eval (Take 10 =<< Nats) :~: '[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+_testNats = Refl
 
 data Fibs :: Exp (Stream Nat)
 
@@ -81,8 +82,101 @@ data FibF :: (Nat, Nat) -> Exp (Nat, (Nat, Nat))
 
 type instance Eval (FibF '(a, b)) = '(a, '(b, a TN.+ b))
 
-testFibs :: Eval (Nth 100 =<< Fibs) :~: 354224848179261915075
-testFibs = Refl
+_testFibs :: Eval (Nth 100 =<< Fibs) :~: 354224848179261915075
+_testFibs = Refl
+
+--------------------------------------------------------------------------------
+-- Regular Language
+
+infixl 1 :+
+
+infixl 2 :.
+
+data Lang a = Lang Bool (a -> Exp (Lang a))
+
+type family Accept (l :: Lang a) :: Bool where
+  Accept ('Lang a _) = a
+
+type family Deriv (l :: Lang a) (x :: a) :: Lang a where
+  Deriv ('Lang _ d) x = d @@ x
+
+type family Member (xs :: [a]) (l :: Lang a) :: Bool where
+  Member '[] l = Accept l
+  Member (x ': xs) l = Member xs (Deriv l x)
+
+type Empty = 'Lang 'False EmptyD
+
+data EmptyD :: a -> Exp (Lang a)
+
+type instance Eval (EmptyD _) = Empty
+
+type Epsilon = 'Lang 'True EpsilonD
+
+data EpsilonD :: a -> Exp (Lang a)
+
+type instance Eval (EpsilonD _) = Empty
+
+type family Single (x :: a) :: Lang a where
+  Single x = 'Lang 'False (SingleD x)
+
+data SingleD :: a -> a -> Exp (Lang a)
+
+type instance Eval (SingleD x y) = If (Eval (TyEq x y)) Epsilon Empty
+
+type family (l1 :: Lang a) :+ (l2 :: Lang a) :: Lang a where
+  l1 :+ l2 = 'Lang (Accept l1 TB.|| Accept l2) (UnionD l1 l2)
+
+data UnionD :: Lang a -> Lang a -> a -> Exp (Lang a)
+
+type instance Eval (UnionD l1 l2 x) = Deriv l1 x :+ Deriv l2 x
+
+type family (l1 :: Lang a) :. (l2 :: Lang a) :: Lang a where
+  l1 :. l2 = 'Lang (Accept l1 TB.&& Accept l2) (ConcatD l1 l2)
+
+data ConcatD :: Lang a -> Lang a -> a -> Exp (Lang a)
+
+type instance
+  Eval (ConcatD l1 l2 x) =
+    If
+      (Accept l1)
+      (Deriv l1 x :. l2 :+ Deriv l2 x)
+      (Deriv l1 x :. l2)
+
+type family Kleene (l :: Lang a) :: Lang a where
+  Kleene l = 'Lang 'True (KleeneD l)
+
+data KleeneD :: Lang a -> a -> Exp (Lang a)
+
+type instance Eval (KleeneD l x) = Deriv l x :. Kleene l
+
+-- Example
+
+type family OneOf (xs :: [a]) :: Lang a where
+  OneOf '[] = Empty
+  OneOf (x ': xs) = Single x :+ OneOf xs
+
+type Zero = Single '0'
+
+type NonZero = OneOf '[ '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+type Digit = Zero :+ NonZero
+
+type Number = Zero :+ NonZero :. Kleene Digit
+
+_testNumber1 :: Member '[] Number :~: 'False
+_testNumber1 = Refl
+
+_testNumber2 :: Member '[ '0'] Number :~: 'True
+_testNumber2 = Refl
+
+_testNumber3 :: Member '[ '1'] Number :~: 'True
+_testNumber3 = Refl
+
+_testNumber4 :: Member '[ '0', '1'] Number :~: 'False
+_testNumber4 = Refl
+
+_testNumber5 :: Member '[ '1', '0', '0'] Number :~: 'True
+_testNumber5 = Refl
 
 --------------------------------------------------------------------------------
 -- Fixpoint
@@ -91,8 +185,8 @@ data Fix :: (Exp a -> Exp a) -> Exp a
 
 type instance Eval (Fix f) = Eval (f (Fix f))
 
-testFix :: Eval (Fix (ConstFn 1)) :~: 1
-testFix = Refl
+_testFix :: Eval (Fix (ConstFn 1)) :~: 1
+_testFix = Refl
 
 --------------------------------------------------------------------------------
 
