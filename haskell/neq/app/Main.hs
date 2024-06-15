968022ea49d90822ae56b12e267344cef7274ca2
wasabi315 <wasabi.nvim.st@gmail.com>
2024-06-15 19:09:23 +0900

    Update neq

--- haskell/neq/app/Main.hs
+++ haskell/neq/app/Main.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeFamilies #-}
 
 import Data.Kind
@@ -7,13 +6,12 @@ import GHC.TypeError
 
 infix 4 /~
 
-type (/~) :: forall k. k -> k -> Constraint
+type (/~) :: k -> k -> Constraint
 type family a /~ b where
   a /~ a = Unsatisfiable ('Text "Expected different types, but got the same type: " :<>: 'ShowType a)
   _ /~ _ = ()
 
-data Void where
-  Void :: (() /~ ()) => Void
+data Void = (Void /~ Void) => Void
 
 ex :: Void -> a
 ex f = case f of {}
